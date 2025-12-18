ca20c560a12da288d8f715a3c1420984839921fe
Jeferson Eiji <jeferson.eiji@icloud.com>
2025-12-18 20:02:56 -0300

    build: transpiled and added multi-stage build for shipping

---
+++ orchestrated/microservices/shipping/rspack.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from '@rspack/cli';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const file_path = fileURLToPath(import.meta.url);
+const current_dir = dirname(file_path);
+
+export default defineConfig({
+    entry: { main: './consumer.ts' },
+    output: { path: current_dir + '/dist' },
+    externals: { 'pg-native': 'commonjs pg-native' },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                loader: 'builtin:swc-loader',
+                options: {
+                    jsc: {
+                        parser: {
+                            syntax: 'typescript',
+                        },
+                    },
+                },
+                type: 'javascript/auto',
+            },
+        ],
+    },
+});
