f8b5b2bbe880bd4150c912cadce988357daa41ac
Jeferson Eiji <jeferson.eiji@icloud.com>
2025-12-18 18:24:28 -0300

    build: dockerized payment microservice

--- orchestrated/microservices/payment/compensate.ts
+++ orchestrated/microservices/payment/compensate.ts
@@ -1,45 +1,81 @@
 import amqp from 'amqplib/callback_api';
 
-amqp.connect('amqp://localhost', (err, conn) => {
-    if (err) {
-        throw err;
-    }
+// amqp.connect('amqp://localhost', (err, conn) => {
+//     if (err) {
+//         throw err;
+//     }
 
-    conn.createChannel((e, channel) => {
-        if (e) {
-            throw e;
-        }
+//     conn.createChannel((e, channel) => {
+//         if (e) {
+//             throw e;
+//         }
 
-        const exchange = 'payment';
-        const routing_key = 'payment.refund';
-        const queue = 'compensate.payment';
+//         const exchange = 'payment';
+//         const routing_key = 'payment.refund';
+//         const queue = 'compensate.payment';
 
-        channel.assertExchange(exchange, 'topic', { durable: true });
-        channel.assertQueue(queue, { durable: true });
-        channel.bindQueue(queue, exchange, routing_key);
+//         channel.assertExchange(exchange, 'topic', { durable: true });
+//         channel.assertQueue(queue, { durable: true });
+//         channel.bindQueue(queue, exchange, routing_key);
 
-        channel.consume(queue, async msg => {
-            const raw = msg?.content.toString();
-            const command = JSON.parse(raw!);
-            console.log('[Payment] Received command: ', command);
+//         channel.consume(queue, async msg => {
+//             const raw = msg?.content.toString();
+//             const command = JSON.parse(raw!);
+//             console.log('[Payment] Received command: ', command);
 
-            channel.ack(msg!);
+//             channel.ack(msg!);
 
-            channel.publish(
-                'orchestrator.events',
-                'saga.reply.compensate.payment',
-                Buffer.from(
-                    JSON.stringify({
-                        order_id: '12645',
-                        amount: 10,
-                        user_id: '1564ga',
-                        success: true
-                    })),
-                {
-                    correlationId: msg?.properties.correlationId as any,
-                    type: 'compensate_refund_payment'
-                }
-            );
-        });
+//             channel.publish(
+//                 'orchestrator.events',
+//                 'saga.reply.compensate.payment',
+//                 Buffer.from(
+//                     JSON.stringify({
+//                         order_id: '12645',
+//                         amount: 10,
+//                         user_id: '1564ga',
+//                         success: true
+//                     })),
+//                 {
+//                     correlationId: msg?.properties.correlationId as any,
+//                     type: 'compensate_refund_payment'
+//                 }
+//             );
+//         });
+//     });
+// });
+
+export const compensate = (e: any, channel: amqp.Channel) => {
+    if (e) throw e;
+    
+    const exchange = 'payment';
+    const routing_key = 'payment.refund';
+    const queue = 'compensate.payment';
+
+    channel.assertExchange(exchange, 'topic', { durable: true });
+    channel.assertQueue(queue, { durable: true });
+    channel.bindQueue(queue, exchange, routing_key);
+
+    channel.consume(queue, async msg => {
+        const raw = msg?.content.toString();
+        const command = JSON.parse(raw!);
+        console.log('[Payment] Received command: ', command);
+
+        channel.ack(msg!);
+
+        channel.publish(
+            'orchestrator.events',
+            'saga.reply.compensate.payment',
+            Buffer.from(
+                JSON.stringify({
+                    order_id: '12645',
+                    amount: 10,
+                    user_id: '1564ga',
+                    success: true
+                })),
+            {
+                correlationId: msg?.properties.correlationId as any,
+                type: 'compensate_refund_payment'
+            }
+        );
     });
-});
+};
--- orchestrated/microservices/payment/consumer.ts
+++ orchestrated/microservices/payment/consumer.ts
@@ -1,44 +1,79 @@
 import amqp from 'amqplib/callback_api';
 
-amqp.connect('amqp://localhost', (err, conn) => {
-    if (err) {
-        throw err;
-    }
+// amqp.connect('amqp://localhost', (err, conn) => {
+//     if (err) {
+//         throw err;
+//     }
 
-    conn.createChannel((e, channel) => {
-        if (e) {
-            throw e;
-        }
+//     conn.createChannel((e, channel) => {
+//         if (e) {
+//             throw e;
+//         }
 
-        const exchange = 'payment';
-        const routing_key = 'payment.charge';
-        const queue = 'commands.payment';
+//         const exchange = 'payment';
+//         const routing_key = 'payment.charge';
+//         const queue = 'commands.payment';
 
-        channel.assertExchange(exchange, 'topic', { durable: true });
-        channel.assertQueue(queue, { durable: true });
-        channel.bindQueue(queue, exchange, routing_key);
+//         channel.assertExchange(exchange, 'topic', { durable: true });
+//         channel.assertQueue(queue, { durable: true });
+//         channel.bindQueue(queue, exchange, routing_key);
 
-        channel.consume(queue, async msg => {
-            const raw = msg?.content.toString();
-            const command = JSON.parse(raw!);
-            console.log('[Payment] Received command: ', command);
+//         channel.consume(queue, async msg => {
+//             const raw = msg?.content.toString();
+//             const command = JSON.parse(raw!);
+//             console.log('[Payment] Received command: ', command);
 
-            channel.ack(msg!);
+//             channel.ack(msg!);
 
-            channel.publish(
-                'orchestrator.events',
-                'saga.reply.payment.success',
-                Buffer.from(
-                    JSON.stringify({
-                        order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
-                        user_id: "6942bd701485b0c89623f19e",
-                        success: true
-                    })),
-                {
-                    correlationId: msg?.properties.correlationId as any,
-                    type: 'process_payment'
-                }
-            );
-        });
+//             channel.publish(
+//                 'orchestrator.events',
+//                 'saga.reply.payment.success',
+//                 Buffer.from(
+//                     JSON.stringify({
+//                         order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
+//                         user_id: "6942bd701485b0c89623f19e",
+//                         success: true
+//                     })),
+//                 {
+//                     correlationId: msg?.properties.correlationId as any,
+//                     type: 'process_payment'
+//                 }
+//             );
+//         });
+//     });
+// });
+
+export const consumer = (e: any, channel: amqp.Channel) => {
+    if (e) throw e;
+
+    const exchange = 'payment';
+    const routing_key = 'payment.charge';
+    const queue = 'commands.payment';
+
+    channel.assertExchange(exchange, 'topic', { durable: true });
+    channel.assertQueue(queue, { durable: true });
+    channel.bindQueue(queue, exchange, routing_key);
+
+    channel.consume(queue, async msg => {
+        const raw = msg?.content.toString();
+        const command = JSON.parse(raw!);
+        console.log('[Payment] Received command: ', command);
+
+        channel.ack(msg!);
+
+        channel.publish(
+            'orchestrator.events',
+            'saga.reply.payment.success',
+            Buffer.from(
+                JSON.stringify({
+                    order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
+                    user_id: "6942bd701485b0c89623f19e",
+                    success: true
+                })),
+            {
+                correlationId: msg?.properties.correlationId as any,
+                type: 'process_payment'
+            }
+        );
     });
-});
+};
