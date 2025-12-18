5096106728ed6101448aa9c466081d97cdf84d74
Jeferson Eiji <jeferson.eiji@icloud.com>
2025-12-18 17:49:18 -0300

    build: dockerized invoice microservice

--- orchestrated/microservices/invoice/compensate.ts
+++ orchestrated/microservices/invoice/compensate.ts
@@ -1,45 +1,81 @@
 import amqp from 'amqplib/callback_api';
 
-amqp.connect('amqp://localhost', (err, conn) => {
-    if (err) {
-        throw err;
-    }
+// amqp.connect('amqp://localhost', (err, conn) => {
+//     if (err) {
+//         throw err;
+//     }
 
-    conn.createChannel((e, channel) => {
-        if (e) {
-            throw e;
-        }
+//     conn.createChannel((e, channel) => {
+//         if (e) {
+//             throw e;
+//         }
 
-        const exchange = 'billing';
-        const routing_key = 'invoice.revoke';
-        const queue = 'compensate.billing';
+//         const exchange = 'billing';
+//         const routing_key = 'invoice.revoke';
+//         const queue = 'compensate.billing';
 
-        channel.assertExchange(exchange, 'topic', { durable: true });
-        channel.assertQueue(queue, { durable: true });
-        channel.bindQueue(queue, exchange, routing_key);
+//         channel.assertExchange(exchange, 'topic', { durable: true });
+//         channel.assertQueue(queue, { durable: true });
+//         channel.bindQueue(queue, exchange, routing_key);
 
-        channel.consume(queue, async msg => {
-            const raw = msg?.content.toString();
-            const command = JSON.parse(raw!);
-            console.log('[Invoice] Received command: ', command);
+//         channel.consume(queue, async msg => {
+//             const raw = msg?.content.toString();
+//             const command = JSON.parse(raw!);
+//             console.log('[Invoice] Received command: ', command);
 
-            channel.ack(msg!);
+//             channel.ack(msg!);
 
-            channel.publish(
-                'orchestrator.events',
-                'saga.reply.compensate.billing',
-                Buffer.from(
-                    JSON.stringify({
-                        order_id: '12645',
-                        amount: 35,
-                        user_id: '1564ga',
-                        success: true
-                    })),
-                {
-                    correlationId: msg?.properties.correlationId as any,
-                    type: 'compensate_revoke_invoice'
-                }
-            );
-        });
+//             channel.publish(
+//                 'orchestrator.events',
+//                 'saga.reply.compensate.billing',
+//                 Buffer.from(
+//                     JSON.stringify({
+//                         order_id: '12645',
+//                         amount: 35,
+//                         user_id: '1564ga',
+//                         success: true
+//                     })),
+//                 {
+//                     correlationId: msg?.properties.correlationId as any,
+//                     type: 'compensate_revoke_invoice'
+//                 }
+//             );
+//         });
+//     });
+// });
+
+export const compensate = (e: any, channel: amqp.Channel) => {
+    if (e) throw e;
+
+    const exchange = 'billing';
+    const routing_key = 'invoice.revoke';
+    const queue = 'compensate.billing';
+
+    channel.assertExchange(exchange, 'topic', { durable: true });
+    channel.assertQueue(queue, { durable: true });
+    channel.bindQueue(queue, exchange, routing_key);
+
+    channel.consume(queue, async msg => {
+        const raw = msg?.content.toString();
+        const command = JSON.parse(raw!);
+        console.log('[Invoice] Received command: ', command);
+
+        channel.ack(msg!);
+
+        channel.publish(
+            'orchestrator.events',
+            'saga.reply.compensate.billing',
+            Buffer.from(
+                JSON.stringify({
+                    order_id: '12645',
+                    amount: 35,
+                    user_id: '1564ga',
+                    success: true
+                })),
+            {
+                correlationId: msg?.properties.correlationId as any,
+                type: 'compensate_revoke_invoice'
+            }
+        );
     });
-});
+};
--- orchestrated/microservices/invoice/consumer.ts
+++ orchestrated/microservices/invoice/consumer.ts
@@ -1,44 +1,79 @@
 import amqp from 'amqplib/callback_api';
 
-amqp.connect('amqp://localhost', (err, conn) => {
-    if (err) {
-        throw err;
-    }
+// amqp.connect('amqp://localhost', (err, conn) => {
+//     if (err) {
+//         throw err;
+//     }
 
-    conn.createChannel((e, channel) => {
-        if (e) {
-            throw e;
-        }
+//     conn.createChannel((e, channel) => {
+//         if (e) {
+//             throw e;
+//         }
 
-        const exchange = 'billing';
-        const routing_key = 'invoice.generate';
-        const queue = 'commands.billing';
+//         const exchange = 'billing';
+//         const routing_key = 'invoice.generate';
+//         const queue = 'commands.billing';
 
-        channel.assertExchange(exchange, 'topic', { durable: true });
-        channel.assertQueue(queue, { durable: true });
-        channel.bindQueue(queue, exchange, routing_key);
+//         channel.assertExchange(exchange, 'topic', { durable: true });
+//         channel.assertQueue(queue, { durable: true });
+//         channel.bindQueue(queue, exchange, routing_key);
 
-        channel.consume(queue, async msg => {
-            const raw = msg?.content.toString();
-            const command = JSON.parse(raw!);
-            console.log('[Invoice] Received command: ', command);
+//         channel.consume(queue, async msg => {
+//             const raw = msg?.content.toString();
+//             const command = JSON.parse(raw!);
+//             console.log('[Invoice] Received command: ', command);
 
-            channel.ack(msg!);
+//             channel.ack(msg!);
 
-            channel.publish(
-                'orchestrator.events',
-                'saga.reply.billing.success',
-                Buffer.from(
-                    JSON.stringify({
-                        order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
-                        address: '5th Avenue, New York, NY, USA',
-                        success: true
-                    })),
-                {
-                    correlationId: msg?.properties.correlationId as any,
-                    type: 'generate_invoice'
-                }
-            );
-        });
+//             channel.publish(
+//                 'orchestrator.events',
+//                 'saga.reply.billing.success',
+//                 Buffer.from(
+//                     JSON.stringify({
+//                         order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
+//                         address: '5th Avenue, New York, NY, USA',
+//                         success: true
+//                     })),
+//                 {
+//                     correlationId: msg?.properties.correlationId as any,
+//                     type: 'generate_invoice'
+//                 }
+//             );
+//         });
+//     });
+// });
+
+export const consumer = (e: any, channel: amqp.Channel) => {
+    if (e) throw e;
+
+    const exchange = 'billing';
+    const routing_key = 'invoice.generate';
+    const queue = 'commands.billing';
+
+    channel.assertExchange(exchange, 'topic', { durable: true });
+    channel.assertQueue(queue, { durable: true });
+    channel.bindQueue(queue, exchange, routing_key);
+
+    channel.consume(queue, async msg => {
+        const raw = msg?.content.toString();
+        const command = JSON.parse(raw!);
+        console.log('[Invoice] Received command: ', command);
+
+        channel.ack(msg!);
+
+        channel.publish(
+            'orchestrator.events',
+            'saga.reply.billing.success',
+            Buffer.from(
+                JSON.stringify({
+                    order_id: "816c64e0-60ac-4a4e-bfa6-ca18914df0ed",
+                    address: '5th Avenue, New York, NY, USA',
+                    success: true
+                })),
+            {
+                correlationId: msg?.properties.correlationId as any,
+                type: 'generate_invoice'
+            }
+        );
     });
-});
+};
